dbfea0ef3f7e73158d215737b8b98791bc119835
Ascastillo98 <ascastillo1998@gmail.com>
2023-11-15 10:38:36 +0100

    commit de prueba

--- nuevoprimercodigo.java
+++ nuevoprimercodigo.java
@@ -1,7 +1,7 @@
 public class nuevoprimercodigo(){
     public static void main(){
         for(int i = 0; i < 10; i++){
-            System.out.println("hola");
+            System.out.println("hola de nuevo");
         }
     }
 }
