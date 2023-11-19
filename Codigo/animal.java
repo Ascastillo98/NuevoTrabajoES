959b99a655a0e78c209293ac44d7c0947602a7b8
Ascastillo98 <ascastillo1998@gmail.com>
2023-11-19 16:02:31 +0100

    Añadido codigo ejemplo

---
+++ Codigo/animal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class animal{
+        public int id;
+        public String especie;
+        public int edad;
+        public String color;
+        public int peso;
+        public animal(int i, String s, int e, String c, int p) {
+            id = i;
+            especie = s;
+            edad = e;
+            color = c;
+            peso = p;
+        }
+        public void verAnimal(){
+            System.out.println("Especie: " + especie);
+            System.out.println("edad: " + edad);
+            System.out.println("color: " + color);
+            System.out.println("peso: " + peso);
+        }
+}
