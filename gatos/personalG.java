67710bb3d223294b9562098a222e899bb790ae51
Ascastillo98 <ascastillo1998@gmail.com>
2023-11-22 17:36:58 +0100

    nuevo fichero

---
+++ gatos/personalG.java
@@ -0,0 +1,24 @@
+public class personalG{
+    private int id;
+    private String nombre;
+    private int edad;
+    private String puesto;
+    private boolean indefinido;
+
+    public personalG(int i, String n, int e, String p, boolean ind){
+        id = i;
+        nombre = n;
+        edad = e;
+        puesto = p;
+        indefinido = ind;
+    }
+
+    public verdatos(){
+        logger.info("id: ", id);
+        logger.info("nombre: ", n);
+        logger.info("edad: ", edad);
+        logger.info("puesto: ", puesto);
+        logger.info("indefinido: ", indefinido);
+
+    }
+}
